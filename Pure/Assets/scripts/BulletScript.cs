3868dd9c95fe1e224321afbacab0ab5c557d4cfd
TheSmallStar <52208581+pure-game@users.noreply.github.com>
2019-07-01 16:42:06 +0500

    new pr

---
+++ Pure/Assets/scripts/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] float damage = 10;
+
+    GameObject parent;
+
+    public float Damage { get => damage; set => damage = value; }
+    public GameObject Parent { get => parent; set => parent = value; }
+
+    public void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if ((collider2D.gameObject.GetComponent<Entity>() == null || collider2D.gameObject.GetComponent<Entity>().fraction != parent.GetComponent<Entity>().fraction)
+            && collider2D.gameObject.GetComponent<Entity>().Bullet == false)
+        {
+            if (collider2D.gameObject.GetComponent<HpScript>() != null) {
+                collider2D.gameObject.GetComponent<HpScript>().Damaged(damage);
+            }
+            Destroy(gameObject);//уничтожаем объект со скриптом
+        }
+    }
+}
