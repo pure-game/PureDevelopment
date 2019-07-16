55cc8a924c4172abecfc4ee672b28b2fe15aa29e
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-17 00:48:49 +0500

    +++PROCEDUREGENERATION

---
+++ PureLast/Assets/scripts/Procedure.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Procedure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Vector2 spawnPosition;
+    [SerializeField] int sectionsCount;
+    [SerializeField] int sectionWidth;
+    [SerializeField] Transform player;
+    [SerializeField] GameObject section;
+
+    Queue<GameObject> sectionsQueue = new Queue<GameObject>();
+    float distance = 0f;
+    float distanceToSpawn = 0f;
+
+    void Start()
+    {
+        for (int i = 0; i < sectionsCount; i++)
+        {
+            spawnNewSection();
+        }
+        distance = Mathf.Abs(spawnPosition.x - player.position.x);
+        distanceToSpawn = sectionWidth / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distance - Mathf.Abs(spawnPosition.x - player.position.x) > distanceToSpawn)
+        {
+            spawnNewSection();
+            Destroy(sectionsQueue.Dequeue());
+        }
+    }
+
+    void spawnNewSection()
+    {
+        GameObject sec = Instantiate(section, spawnPosition, Quaternion.identity, transform) as GameObject;
+        sectionsQueue.Enqueue(sec);
+        spawnPosition.x += sectionWidth;
+    }
+
+}
