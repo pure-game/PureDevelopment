18a6f2578e0f9f47fc237c08e01d17a058976c8b
TheSmallStar <52208581+pure-game@users.noreply.github.com>
2019-08-03 04:59:39 +0500

    +++SlowPuddle

--- PureLast/Assets/Scripts/SlowPuddleScript.cs
+++ PureLast/Assets/Scripts/SlowPuddleScript.cs
@@ -5,24 +5,17 @@ using UnityEngine;
 // скрипт для замедляющей лужи
 public class SlowPuddleScript : MonoBehaviour
 {
-    [SerializeField] public float Damage;
-
-    List<ObjectStats> objects = new List<ObjectStats>();
-
-    void Start()
-    {
-        StartCoroutine(DamageObjects());
-    }
+    [SerializeField] public float slowMultiplier = 0.5f;
 
     // Наносим урон всем объектам, попадающим под действие газа
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.isTrigger)
             return;
-        ObjectStats other = collision.GetComponent<ObjectStats>();
+        MovementController other = collision.GetComponent<MovementController>();
         if (other != null)
         {
-            objects.Add(other);
+            other.changeVelocity(slowMultiplier);
         }
     }
 
@@ -30,23 +23,11 @@ public class SlowPuddleScript : MonoBehaviour
     {
         if (collision.isTrigger)
             return;
-        ObjectStats other = collision.GetComponent<ObjectStats>();
+        MovementController other = collision.GetComponent<MovementController>();
         if (other != null)
         {
-            objects.Remove(other);
+            other.resetVelocity();
         }
     }
 
-    IEnumerator DamageObjects()
-    {
-        while (true)
-        {
-            for (int i = 0; i < objects.Count; i++)
-            {
-
-                objects[i].Damaged(Damage);
-            }
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
 }
