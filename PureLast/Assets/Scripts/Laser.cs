9db0a2690ff1fbac153705f6f962662c946f387f
TheSmallStar <52208581+pure-game@users.noreply.github.com>
2019-08-08 02:41:15 +0500

    +++LaserSample

---
+++ PureLast/Assets/Scripts/Laser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] float length = 100;
+    [SerializeField] Transform tip;
+
+    Vector3 defaultScale;
+
+    void Start()
+    {
+        defaultScale = transform.localScale;
+    }
+
+    
+    void Update()
+    {
+        CheckObstacles();
+    }
+
+    void CheckObstacles()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, length);
+        Vector3 end = transform.position + transform.right * length;
+        Vector3 scale = new Vector3(length, defaultScale.y, defaultScale.z);
+
+        if (hit.collider != null)
+        {
+            end = hit.point;
+            print(hit.point);
+            scale.x = hit.distance;
+        }
+        tip.position = end;
+        transform.localScale = scale;
+    }
+}
