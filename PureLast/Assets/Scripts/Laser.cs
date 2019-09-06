4ced051d1cc81d8350326e5228dca5f68a856a5e
VserosBuyBuy <52208581+pure-game@users.noreply.github.com>
2019-09-06 18:13:34 +0300

    +++LaserUpdated

--- PureLast/Assets/Scripts/Laser.cs
+++ PureLast/Assets/Scripts/Laser.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [SerializeField] float length = 100;
+    [SerializeField] float rotationVeloctyInDegrees = 0f;
     [SerializeField] Transform tip;
+    [SerializeField] Transform laserPlasma;
+    [SerializeField] float DPS = 20f;
+    [SerializeField] float damageDeltaTime = 0.2f;
 
     Vector3 defaultScale;
+    Vector3 rotationAngle;
+    PlayerStats player;
 
     void Start()
     {
-        defaultScale = transform.localScale;
+        defaultScale = laserPlasma.localScale;
+        rotationAngle = new Vector3(0, 0, rotationVeloctyInDegrees);
+        StartCoroutine(damage());
     }
 
     
     void Update()
     {
         CheckObstacles();
+        if (rotationVeloctyInDegrees != 0)
+        {
+            var oldRot = transform.rotation;
+            transform.Rotate(rotationAngle);
+        }
     }
 
     void CheckObstacles()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, length);
-        Vector3 end = transform.position + transform.right * length;
+        RaycastHit2D hit = Physics2D.Raycast(laserPlasma.position, laserPlasma.right, length);
+        Vector3 end = laserPlasma.position + laserPlasma.right * length;
         Vector3 scale = new Vector3(length, defaultScale.y, defaultScale.z);
 
         if (hit.collider != null)
@@ -31,8 +44,22 @@ public class Laser : MonoBehaviour
             end = hit.point;
             print(hit.point);
             scale.x = hit.distance;
+            player = hit.collider.GetComponent<PlayerStats>();
         }
         tip.position = end;
-        transform.localScale = scale;
+        laserPlasma.localScale = scale;
     }
+
+    IEnumerator damage()
+    {
+        while(true)
+        {
+            if (player != null)
+            {
+                player.Damaged(DPS * damageDeltaTime);
+            }
+            yield return new WaitForSeconds(damageDeltaTime);
+        }
+    }
+
 }
